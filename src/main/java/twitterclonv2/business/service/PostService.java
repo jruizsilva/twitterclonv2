629501ea7823461ceca3090d6dbe5bbcb5831239
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-02 11:31:07 -0300

    removing methods not used in PostService

--- src/main/java/twitterclonv2/business/service/PostService.java
+++ src/main/java/twitterclonv2/business/service/PostService.java
@@ -7,13 +7,10 @@ import java.util.List;
 
 public interface PostService {
     PostEntity createOnePost(PostRequest postRequest);
-    List<PostEntity> findAll();
     List<PostEntity> findByOrderByCreatedAtDesc();
     PostEntity updatePost(PostRequest postRequest,
                           Long postId);
     void deletePostById(Long postId);
-    PostEntity findPostById(Long postId);
-    PostEntity savePost(PostEntity postEntity);
     PostEntity addLikeToPost(Long postId);
     PostEntity removeLikeInPost(Long postId);
 }
